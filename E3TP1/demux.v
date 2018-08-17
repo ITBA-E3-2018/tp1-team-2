012b14581f7611bb462c2048d2ff4466ab4fa6a1
ohvictor <voh@itba.edu.ar>
2018-08-17 13:04:02 -0300

    Add files via upload

--- E3TP1/demux.v
+++ E3TP1/demux.v
@@ -1,26 +1,38 @@
 module demux(
-    demux_in,       //Entrada de la senal
-    demux_select,   //Entrada de selector
-    demux_out       //Salida de la senal
+    d_in,       //Entrada de la senal
+    d_sel,   //Entrada de selector
+    d_out_0,       //Salida de la senal
+    d_out_1,
+    d_out_2,
+    d_out_3
 );
 
 //Declaracion de entradas
-input demux_in;
+input d_in;
 
 //Declaracion de salidas
-output [3:0] demux_out;
+output d_out_0, d_out_1, d_out_2, d_out_3;
 
 //Supuestamente esta seria inout?
-input [1:0] demux_select;
+input [1:0] d_sel;
 
 //Supongo que esto tambien se hace.
-wire demux_in;
-wire [1:0] demux_select;
-reg [3:0] demux_out;
+wire d_in;
+wire [1:0] d_sel;
+reg d_out_0, d_out_1, d_out_2, d_out_3;
 
-always @ (demux_in) begin
-  demux_out = 0;    //Limpio lo que estaba antes
-  demux_out[demux_select] = demux_in;
+always @ (d_in) begin
+  d_out_0 = 0;    //Limpio lo que estaba antes
+  d_out_1 = 0;
+  d_out_2 = 0;
+  d_out_3 = 0;
+
+  case (d_sel)
+    0: d_out_0 = d_in;
+    1: d_out_1 = d_in;
+    2: d_out_2 = d_in;
+    3: d_out_3 = d_in;
+  endcase
 end
 
 endmodule
--- E3TP1/demux_tb.v
+++ E3TP1/demux_tb.v
@@ -4,12 +4,20 @@ module demux_tb;
 
 reg data_in;
 reg [1:0] data_select;
-wire [3:0] data_out;
+wire o0,o1,o2,o3;
 
-demux e_0(.demux_in(data_in),.demux_select(data_select),.demux_out(data_out));
+demux e_0(
+    .d_in(data_in),
+    .d_sel(data_select),
+    .d_out_0(o0),
+    .d_out_1(o1),
+    .d_out_2(o2),
+    .d_out_3(o3)
+    );
 
 initial begin
     data_in = 1;
+    data_select = 0;
 end
 
 always begin
@@ -26,8 +34,8 @@ initial begin
 end
 
 initial begin
-  $display("Input value\tSelector\tOutput");
-  $monitor("%b\t\t%d\t\t%b",data_in, data_select, data_out);
+  $display("Input\tSelector\tOut_0\tOut_1\tOut_2\tOut_3");
+  $monitor("%b\t%d\t\t%d\t%d\t%d\t%d",data_in, data_select, o0,o1,o2,o3);
 end
 
 endmodule
