dee261a820894bf753f90bb4e754ac21e960962a
idiaz97 <idiaz@itba.edu.ar>
2018-08-20 13:51:41 -0300

    Changed verilog code, I confused AND gates with OR gates

--- E4TP1/E4TP1.v
+++ E4TP1/E4TP1.v
@@ -8,28 +8,28 @@ module twosComplement (x1,x2,x3,x4,f1,f2,f3,f4);
     not(nx4,x4);
     //First Bit Logic
     wire temp1, temp2, temp3;
-    or(temp1,x1,nx2,nx3,nx4);
-    and(temp2,x2,x3,x4);
-    or(temp3,temp2,nx1);
-    and(f1,temp1,temp3); //First Bit output
+    and(temp1,x1,nx2,nx3,nx4);
+    or(temp2,x2,x3,x4);
+    and(temp3,temp2,nx1);
+    or(f1,temp1,temp3); //First Bit output
 
     //Second Bit Logic
     wire t1,t2,t3;
-    or(t1,x2,nx3,nx4);
-    and(t2,x3,x4);
-    or(t3,t2,nx2);
-    and(f2,t1,t3); //Second Bit Output
+    and(t1,x2,nx3,nx4);
+    or(t2,x3,x4);
+    and(t3,t2,nx2);
+    or(f2,t1,t3); //Second Bit Output
 
     //Third Bit Logic
     wire r1,r2;
-    or(r1,x3,nx4);
-    or(r2,nx3,x4);
-    and(f3,r1,r2); //Third Bit Output
+    and(r1,x3,nx4);
+    and(r2,nx3,x4);
+    or(f3,r1,r2); //Third Bit Output
 
     //Four Bit Output
     wire q;
-    or(q,nx1,nx2,nx3);
-    and(f4,x4,q); //Four Bit Output
+    and(q,nx1,nx2,nx3);
+    or(f4,x4,q); //Four Bit Output
 
 
 endmodule
