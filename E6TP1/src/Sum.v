1aaf64a9ded1b442a1d819c75d89522505cd6f10
idiaz97 <idiaz@itba.edu.ar>
2018-08-20 21:38:36 -0300

    Compila Sum.v pero algunas sumas no dan bien

--- E6TP1/src/Sum.v
+++ E6TP1/src/Sum.v
@@ -1,44 +1,35 @@
 module sum (x0,x1,x2,x3,y0,y1,y2,y3,o0,o1,o2,o3,carry);
     input x0,x1,x2,x3,y0,y1,y2,y3;
     output reg o0,o1,o2,o3, carry;
+    wire c3,w;   
+    wire c2b,r,c2a, temp; 
+    wire c1a,c1b,ps1,m1, ps2, m2;
+    wire carryA,carryB, pS1,pS2, f1,f2;  
 
-    wire c3,w;      
     sumBit2Bit FS1 (x3, y3, c3, w);
-    assign o3 = w;                     //Sumo bits X3 y Y3, el output es w y el carry es c3
-
-    reg c2b,r,c2a, temp;
-    sumBit2Bit FS2 (y2,c0,c2a,r);
+    sumBit2Bit FS2 (y2,c3,c2a,r);
     sumBit2Bit FS3 (x2,r,c2b,temp);
-    assign o2 = temp;                  //Sumo X2, Y2 y C3 y me quedan 2 posibles carrys c2a y c2b
+    sumBit2Bit FS4(y1,c2b,c1a,ps1);
+    sumBit2Bit FS5(x1,ps1,c1b,m1);
+    sumBit2Bit FS6(y1,c2a,c1a,ps2);
+    sumBit2Bit FS7(x1,ps2,c1b,m2);
+    sumBit2Bit FS8(y0,c1b,carryA,pS1);
+    sumBit2Bit FS9(x0,pS1,carryB,f1);
+    sumBit2Bit FS10(y0,c1a,carryA,pS2);
+    sumBit2Bit FS11(x0,pS2,carryB,f2);
 
-    reg c1a,c1b,ps1,m1, ps2, m2;
+    
+always @* begin
+    assign o3 = w;                     //Sumo bits X3 y Y3, el output es w y el carry es c3
+    assign o2 = c3;                  //Sumo X2, Y2 y C3 y me quedan 2 posibles carrys c2a y c2b
     if (c2a == 0) 
-    begin
-        sumBit2Bit FS4(y1,c2b,c1a,ps1);
-        sumBit2Bit FS5(x1,ps1,c1b,m1);
         assign o1 = m1;
-    end
     else
-    begin
-        sumBit2Bit FS6(y1,c2a,c1a,ps2);
-        sumBit2Bit FS7(x1,ps2,c1b,m2);
-        assign o1 = m2;
-    end                         //Sumo X1, Y1 y el carry anterior fijandome cual quedo en 1 y descatando el otro
-
-
-    wire carryA,carryB, pS1,pS2, f1,f2;
+        assign o1 = m2;                //Sumo X1, Y1 y el carry anterior fijandome cual quedo en 1 y descatando el otro
     if(c1a == 0) 
-    begin
-        sumBit2Bit FS8(y0,c1b,carryA,pS1);
-        sumBit2Bit FS9(x0,pS1,carryB,f1);
         assign o0 = f1;
-    end
     else
-    begin
-        sumBit2Bit FS10(y0,c1a,carryA,pS2);
-        sumBit2Bit FS11(x0,pS2,carryB,f2);
-        assign o0 = f2;
-    end                         //Sumo X0, Y0 y el carry pertinente de la suma anterior
+        assign o0 = f2;                //Sumo X0, Y0 y el carry pertinente de la suma anterior
 
     if (carryA == 1)
         assign carry = 1;
@@ -46,5 +37,6 @@ module sum (x0,x1,x2,x3,y0,y1,y2,y3,o0,o1,o2,o3,carry);
         assign carry = 1;
     else
         assign carry = 0;       //Me fijo si alguno de los carrys quedo encendido, y si es asi, enciendo el bit del carry
+end
 
 endmodule
--- E6TP1/src/testSum.v
+++ E6TP1/src/testSum.v
@@ -1,9 +1,10 @@
 module testSum;
-    reg in1, in2, in3, in4;
-    wire out1, out2, out3, out4;
+    reg x0,x1,x2,x3,y0,y1,y2,y3;
+    wire o0,o1,o2,o3,o4;
 
-    Sum testModule(x0,x1,x2,x3,y0,y1,y2,y3,o0,o1,o2,o3);
+    sum testModule(x0,x1,x2,x3,y0,y1,y2,y3,o0,o1,o2,o3,o4);
     integer i, j, k, h;
+    integer a, s, d, f;
 
   initial begin
     for (i = 0; i < 2; i = i + 1) begin
@@ -11,30 +12,30 @@ module testSum;
         for (k = 0; k < 2; k = k + 1) begin
             for (h = 0; h < 2; h = h + 1) begin
                 x0 = i; x1 = j; x2 = k; x3 = h;
-                $display("First bit input values are: %b %b %b %b", x0, x1, x2, x3);
+                $display("---------First bit input values are: %b %b %b %b", x0, x1, x2, x3);
+                for (a = 0; a < 2; a = a + 1) begin
+                  for (s = 0; s < 2; s = s + 1) begin
+                    for (d = 0; d < 2; d = d + 1) begin
+                      for (f = 0; f < 2; f = f + 1) begin
+                        y0 = a; y1 = s; y2 = d; y3 = f;
+                        $display("Second bit input values are: %b %b %b %b", y0, y1, y2, y3);
+                        #1;
+                      end
+                    end
+                  end
+                end
                 #1;
             end
         end
       end
     end
 
-    integer a, s, d, f;
 
-    for (a = 0; a < 2; a = a + 1) begin
-      for (s = 0; s < 2; s = s + 1) begin
-        for (d = 0; d < 2; d = d + 1) begin
-            for (f = 0; f < 2; f = f + 1) begin
-                y0 = a; y1 = s; y2 = d; y3 = f;
-                $display("Second bit input values are: %b %b %b %b", y0, y1, y2, y3);
-                #1;
-            end
-        end
-      end
-    end
     $finish;
   end
 
     initial begin
-        $monitor("Outs have changed! New values are: %b %b %b %b", o0, o1, o2, o3) ;
+        $monitor("Outs have changed! New values are: %b %b %b %b  Carry: %b", o0, o1, o2, o3, o4) ;
+        //$monitor("Carry: %b", o4) ;
     end
 endmodule
