001f5a7ba552b3ddc23222c526e68e949e721d53
idiaz97 <idiaz@itba.edu.ar>
2018-08-21 10:18:32 -0300

    Bits on ShiftLeft, are stored in carry bit now

--- E6TP1/src/shiftLeft.v
+++ E6TP1/src/shiftLeft.v
@@ -1,11 +1,12 @@
-module shiftLeft(x0,x1,x2,x3,o0,o1,o2,o3);
+module shiftLeft(x0,x1,x2,x3,o0,o1,o2,o3,carry);
     input x0,x1,x2,x3;
-    output reg o0,o1,o2,o3;
+    output reg o0,o1,o2,o3,carry;
 
     always @(x0 or x1 or x2 or x3) begin
         assign o0 = x1;
         assign o1 = x2;
         assign o2 = x3;
         assign o3 = 0;
+        assign carry = x0;
     end
 endmodule
