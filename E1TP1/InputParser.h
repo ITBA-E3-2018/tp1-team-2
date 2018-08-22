c6ff846c8aa80f43470c7a9601f0a116bdcf25f6
malenamuller <mmuller@itba.edu.ar>
2018-08-21 23:36:13 -0300

    Add files via upload
    Falta probarlo con varios casos, emprolijar código y comentarios

---
+++ E1TP1/InputParser.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "PuntoFijo.h"
+#include <string>
+//#define DEBUG
+
+bool InputParser(int argc, char* argv[], PuntoFijo punto_fijo_);
+bool ItIsBinary(char* sequence);//NO LA USO
+bool ItIsPossitiveInteger(char* sequence);
