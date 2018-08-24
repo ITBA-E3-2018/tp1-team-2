36295cf07dfe4b5d384521437678fb82ecbe31f5
Victor <voh@itba.edu.ar>
2018-08-24 14:57:09 -0300

    Terminado y probado un filtro para invaildar datos que no son BCD

---
+++ E5TP1/filterBCD.v
@@ -0,0 +1,35 @@
+module filterBCD
+(
+    in,     //Entrada de datos
+    out,    //Salida de datos
+    error   //Flag de error o invalido
+);
+
+input [3:0] in;
+
+output[3:0] out;
+output error;
+
+wire [3:0] in;
+reg [3:0] out;
+reg error;
+
+initial begin
+  error = 0;
+  out = 0;
+end
+
+always @ (in) begin
+  if (in>9) begin
+    error = 1;
+    out = 0;
+  end
+  else begin
+    error = 0;
+    out = in;
+  end
+end
+
+
+
+endmodule
