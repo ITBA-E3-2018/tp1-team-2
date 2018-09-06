3aac086c9a13f76799039a2d621852d2a46e50a1
Benjamin Lin <blin@itba.edu.ar>
2018-09-05 21:04:42 -0300

    agregue test

--- E1TP1/test1.sh
+++ E1TP1/test1.sh
@@ -23,6 +23,9 @@ echo "-------------------------"
 echo "Arguments: 0 1000 00000001"
 ./E3_TP1_EJ1.exe 1 1000 00000001
 echo "-------------------------"
+echo "Arguments: 1 132 477789"
+./E3_TP1_EJ1.exe 1 132 477789
+echo "-------------------------"
 
 
 #These cases should show error:
@@ -45,4 +48,13 @@ echo "Arguments: 01 100 1"
 echo "-------------------------"
 echo "Arguments: 0 100M 1"
 ./E3_TP1_EJ1.exe 0 100M 1
+echo "-------------------------"
+echo "Arguments: 0 -1 1"
+./E3_TP1_EJ1.exe 0 -1 1
+echo "-------------------------"
+echo "Arguments: All letters"
+./E3_TP1_EJ1.exe Pepe estuvo aqui
+echo "-------------------------"
+echo "Arguments: No Arguments"
+./E3_TP1_EJ1.exe
 echo "-------------------------"
