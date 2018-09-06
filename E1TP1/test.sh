d29c46a63f251d7a2fe2bf9bdf66a6e2e7966dfa
Ian Diaz <idiaz@itba.edu.ar>
2018-09-05 21:32:41 -0300

    Added makefile

--- E1TP1/test.sh
+++ E1TP1/test.sh
@@ -3,46 +3,46 @@ echo "SHOULD WORK:"
 echo " "
 
 echo "Arguments: 1 1 0"
-./E3_TP1_EJ1.exe 1 1 0
+./executable 1 1 0
 echo "-------------------------"
 echo "Arguments: 1 0 1"
-./E3_TP1_EJ1.exe 1 0 1
+./executable 1 0 1
 echo "-------------------------"
 echo "Arguments: 0 0 1"
-./E3_TP1_EJ1.exe 0 0 1
+./executable 0 0 1
 echo "-------------------------"
 echo "Arguments: 1 0 0"
-./E3_TP1_EJ1.exe 1 0 0
+./executable 1 0 0
 echo "-------------------------"
 echo "Arguments: 0 0 0"
-./E3_TP1_EJ1.exe 0 0 0
+./executable 0 0 0
 echo "-------------------------"
 echo "Arguments: 1 12 0"
-./E3_TP1_EJ1.exe 1 12 0
+./executable 1 12 0
 echo "-------------------------"
 echo "Arguments: 1 3 0"
-./E3_TP1_EJ1.exe 1 3 0
+./executable 1 3 0
 echo "-------------------------"
 echo "Arguments: 1 1  1"
-./E3_TP1_EJ1.exe 1 1  1
+./executable 1 1  1
 echo "-------------------------"
 echo "Arguments: 1 1     1"
-./E3_TP1_EJ1.exe 1 1     1
+./executable 1 1     1
 echo "-------------------------"
 echo "Arguments: 1 3     1"
-./E3_TP1_EJ1.exe 1 3     1
+./executable 1 3     1
 echo "-------------------------"
 echo "Arguments: 1          3     1"
-./E3_TP1_EJ1.exe 1          3     1
+./executable 1          3     1
 echo "-------------------------"
 echo "Arguments: 0 1000 1000"
-./E3_TP1_EJ1.exe 0 1000 1000
+./executable 0 1000 1000
 echo "-------------------------"
 echo "Arguments: 0 1000 00000001"
-./E3_TP1_EJ1.exe 1 1000 00000001
+./executable 1 1000 00000001
 echo "-------------------------"
 echo "Arguments: 0 000 00000001"
-./E3_TP1_EJ1.exe 1 000 00000001
+./executable 1 000 00000001
 echo "-------------------------"
 
 
@@ -50,20 +50,20 @@ echo "-------------------------"
 echo "SHOULDN'T WORK:"
 echo " "
 echo "Arguments: 1 1 -1"
-./E3_TP1_EJ1.exe 1 1 -1
+./executable 1 1 -1
 echo "-------------------------"
 echo "Arguments: 1 1"
-./E3_TP1_EJ1.exe 1 1
+./executable 1 1
 echo "-------------------------"
 echo "Arguments: 0 1000 1001"
-./E3_TP1_EJ1.exe 1 1000 1001
+./executable 1 1000 1001
 echo "-------------------------"
 echo "Arguments: 00 100 1"
-./E3_TP1_EJ1.exe 00 100 1
+./executable 00 100 1
 echo "-------------------------"
 echo "Arguments: 01 100 1"
-./E3_TP1_EJ1.exe 01 100 1
+./executable 01 100 1
 echo "-------------------------"
 echo "Arguments: 0 100M 1"
-./E3_TP1_EJ1.exe 0 100M 1
+./executable 0 100M 1
 echo "-------------------------"
